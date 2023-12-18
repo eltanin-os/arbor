4dfb694d79ef1486ce194128a597d615d3e56dd8
Eduardo Francisco <eadwardus@disroot.org>
2023-12-18 17:08:28 -0300

    www: Add libreselen

---
+++ repo/default/dynamic/www/libreselene/files/libreselene/pref/palemoon-branding.js
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+pref("app.vendorURL", "https://eltaninos.org/");
+pref("browser.identity.ssl_domain_display", 1);
+pref("browser.newtab.url","about:blank");
+//pref("extensions.getMoreThemesURL", "");
+pref("extensions.update.autoUpdateDefault", true);
+pref("extensions.getAddons.maxResults", 10);
+pref("extensions.getAddons.cache.enabled", false);
+pref("dom.max_chrome_script_run_time", 90);
+pref("dom.max_script_run_time", 20);
+pref("plugin.default.state", 2);
+pref("plugin.expose_full_path", true);
+pref("dom.ipc.plugins.timeoutSecs", 20);
+pref("nglayout.initialpaint.delay", 300);
+pref("image.mem.max_ms_before_yield", 50);
+pref("image.mem.decode_bytes_at_a_time", 65536);
+
+pref("accessibility.force_disabled", 1);
+pref("devtools.selfxss.count", 5);
+pref("startup.homepage_welcome_url","https://libreselene.eltaninos.org/welcome.html");
+pref("startup.homepage_override_url","https://libreselene.eltaninos.org/welcome.html");
+//pref("app.releaseNotesURL", "");
+pref("app.update.enabled", false);
+pref("app.update.url", "");
